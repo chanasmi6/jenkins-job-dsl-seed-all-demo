534c251d751e939d6cfc29b9479df51ca4007783
chanasmi6 <34145158+chanasmi6@users.noreply.github.com>
2019-01-13 15:04:22 -0600

    Update Jenkinsfile-call-junit-generator.groovy

--- jobs/Jenkinsfile-call-junit-generator.groovy
+++ jobs/Jenkinsfile-call-junit-generator.groovy
@@ -1,1 +1,18 @@
 
+#!/usr/bin/env groovy
+
+node("master") {
+    def builds = [:]
+    for (int i = 0; i < 10; i++) {
+
+        int index = i
+
+        builds[index] = {
+            build job: 'promote-master', wait: true, propagate: false, quietPeriod: 0
+        }
+    }
+
+    stage("Run promote builds") {
+        parallel builds
+    }
+}
